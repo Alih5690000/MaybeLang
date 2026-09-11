d3d381df87c6bcda32c81a660e20d787bde62753
Қалдыбай Алихан <Qaldybay_A06@krg.nis.edu.kz>
2026-09-11 07:37:43 +0500

    Added multiply and division support

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 #include <string>
 #define LOG(x) std::cout<<x<<std::endl
 
+bool isOnlyOneLayerOfBrackets(const std::string& e){
+    if (e[0]!='(') return false;
+    for (auto i:e){
+        if (i=='(') return false;
+    }
+    return true;
+}
+
+bool hasNoOp(const std::string& e){
+    for (auto i:e){
+        if (i=='+' || i=='-') return false;
+    }
+    return true;
+}
+
 int parseExpression(const std::string& expression) {
     bool onlyNum=true;
+    bool noOp=hasNoOp(expression);
+    LOG(std::string("NO OP IS ")+std::to_string(noOp));
     std::string curr;
     char op='u';
     int sum=0;
@@ -19,8 +36,8 @@ int parseExpression(const std::string& expression) {
         if (expression[i]==')')
             bracketLevel--;
         curr+=expression[i];
-        if ((bracketLevel==0 && (expression[i]=='+' || expression[i]=='-'))
-         || (i==expression.size()-1 && !onlyNum)){
+        if (((bracketLevel==0 && (expression[i]=='+' || expression[i]=='-'))
+         || (i==expression.size()-1 && !onlyNum)) && !noOp){
             LOG("OPERATOR DETECTED");
             if (expression[i]=='+' || expression[i]=='-')
                 curr.pop_back();
@@ -40,6 +57,27 @@ int parseExpression(const std::string& expression) {
             curr.clear();
             continue;
         }
+        if (((bracketLevel==0 && (expression[i]=='/' || expression[i]=='*'))
+        || (i==expression.size()-1 && !onlyNum)) && noOp){
+            LOG("IDK DETECTED");
+            if (expression[i]=='*' || expression[i]=='/')
+                curr.pop_back();
+            if (op=='u'){
+                sum=parseExpression(curr);
+                LOG("FIRST NUM");
+            }
+            if (op=='*'){
+                sum*=parseExpression(curr);
+                LOG("MULTIPLY");
+            }
+            if (op=='/'){
+                LOG("DIVIDE");
+                sum/=parseExpression(curr);
+            }
+            op=expression[i];
+            curr.clear();
+            continue;
+        }
     }
     if (onlyNum){
         LOG("ONLYNUM");
@@ -49,5 +87,5 @@ int parseExpression(const std::string& expression) {
 }
 
 int main(){
-    std::cout<<parseExpression("5+5")<<std::endl;
+    std::cout<<parseExpression("5*5+5")<<std::endl;
 }
